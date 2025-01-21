b17d220d9749b4c50b94c5a26d20288cea13945e
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 13:18:16 +0700

    Add getAll in NameController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/requestlimit/controller/NameController.java
+++ src/main/java/id/my/hendisantika/requestlimit/controller/NameController.java
@@ -3,8 +3,13 @@ package id.my.hendisantika.requestlimit.controller;
 import id.my.hendisantika.requestlimit.service.NameService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,9 @@ class NameController {
 
     private final NameService nameService;
 
+    @GetMapping
+    public ResponseEntity<Mono<List<String>>> getAll() {
+        log.info("====> {}", this.getClass().getName());
+        return ResponseEntity.ok(nameService.getAllNames().collectList());
+    }
 }
