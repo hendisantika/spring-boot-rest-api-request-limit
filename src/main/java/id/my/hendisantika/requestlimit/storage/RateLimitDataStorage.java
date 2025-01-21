6660365df31b24bd1f65d92e36873512eb0e8a8f
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 14:18:01 +0700

    feat: Add RateLimitDataStorage🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/requestlimit/storage/RateLimitDataStorage.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.requestlimit.storage;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-request-limit
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 14.17
+ * To change this template use File | Settings | File Templates.
+ */
+public final class RateLimitDataStorage {
+
+    private static final Map<String, RateLimitData> STORAGE = new ConcurrentHashMap<>();
+}
