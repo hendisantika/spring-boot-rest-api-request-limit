519a08d600ea99d48cc2835af975ead611f1285d
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 13:01:08 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/requestlimit/SpringBootRestApiRequestLimitApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.requestlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiRequestLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiRequestLimitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/requestlimit/SpringBootRestApiRequestLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.requestlimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiRequestLimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
