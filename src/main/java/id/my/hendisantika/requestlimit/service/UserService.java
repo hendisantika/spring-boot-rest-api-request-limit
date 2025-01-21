2f85707de2a6a646faa832839355b2a81160bd6f
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 14:04:03 +0700

    feat: Add UserService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/requestlimit/service/UserService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.requestlimit.service;
+
+import id.my.hendisantika.requestlimit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-request-limit
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 14.03
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+}
