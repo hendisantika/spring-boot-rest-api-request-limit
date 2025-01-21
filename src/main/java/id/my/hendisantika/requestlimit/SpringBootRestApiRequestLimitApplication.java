f6a4f71e6369d8fbb427fad2326780f114c52ce4
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 13:15:14 +0700

    Add Init🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/requestlimit/SpringBootRestApiRequestLimitApplication.java
+++ src/main/java/id/my/hendisantika/requestlimit/SpringBootRestApiRequestLimitApplication.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.requestlimit;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @EnableCaching
 @SpringBootApplication
@@ -12,4 +16,14 @@ public class SpringBootRestApiRequestLimitApplication {
         SpringApplication.run(SpringBootRestApiRequestLimitApplication.class, args);
     }
 
+}
+
+// INIT
+@Log4j2
+@Component
+class Init {
+    @EventListener(ApplicationReadyEvent.class)
+    public void ready() {
+        log.info("====> {}", this.getClass().getName());
+    }
 }
