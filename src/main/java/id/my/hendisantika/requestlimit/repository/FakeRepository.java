5ed2e426f730da4268f5fe3eec7d46deb2595902
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 13:05:36 +0700

    Add init Data in FakeRepository🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/requestlimit/repository/FakeRepository.java
+++ src/main/java/id/my/hendisantika/requestlimit/repository/FakeRepository.java
@@ -4,6 +4,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +23,12 @@ class FakeRepository {
     private static final List<NameData> nameDataStream = new ArrayList<>();
 
     record NameData(String name) {
+    }
+
+    static {
+        nameDataStream.add(new NameData("Yuji"));
+        nameDataStream.add(new NameData("Gojo"));
+        nameDataStream.add(new NameData("Geto"));
+        log.info("====> data loaded {}", nameDataStream.stream().map(NameData::name).collect(Collectors.joining(",")));
     }
 }
