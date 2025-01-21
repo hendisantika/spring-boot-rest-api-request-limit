52fcdde62a0ed233821756c5005610ba6d6447d1
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 14:19:34 +0700

    feat: Add UserRestController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/requestlimit/controller/UserRestController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.requestlimit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-request-limit
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 14.19
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class UserRestController {
+}
