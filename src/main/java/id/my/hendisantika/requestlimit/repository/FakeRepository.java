ee3818ada03ab284ce3811d2855d6487ff2b05a0
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 13:05:10 +0700

    Add FakeRepository🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/requestlimit/repository/FakeRepository.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.requestlimit.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-request-limit
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 13.04
+ * To change this template use File | Settings | File Templates.
+ */
+@Log4j2
+@Component
+class FakeRepository {
+    private static final List<NameData> nameDataStream = new ArrayList<>();
+
+    record NameData(String name) {
+    }
+}
