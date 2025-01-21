42f2ebd555ff3208260ae902312fb6d2d3b91c24
hendisantika <hendisantika@yahoo.co.id>
2025-01-21 14:10:11 +0700

    feat: Add WebRtConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/requestlimit/config/WebRtConfig.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.requestlimit.config;
+
+import com.hazelcast.map.impl.InterceptorRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-request-limit
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/01/25
+ * Time: 14.07
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebRtConfig implements WebMvcConfigurer {
+
+    private final ApiKeyRateLimitRequestInterceptor apiKeyRateLimitRequestInterceptor;
+
+    private final IpRateLimitRequestInterceptor ipRateLimitRequestInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+                .addInterceptor(apiKeyRateLimitRequestInterceptor)
+                .addPathPatterns("/api/v1/rt/api-key");
+        registry.addInterceptor(ipRateLimitRequestInterceptor).addPathPatterns("/api/v1/rt/ip");
+    }
+}
